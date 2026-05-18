a536dc46c0abb5f5e6164ac949c626b5ca4edfc2
developer <yaqublurufat10@gmail.com>
2026-05-18 17:30:26 +0400

    yuklendi

--- src/main/java/com/example/demo/Security/JwtFilter.java
+++ src/main/java/com/example/demo/Security/JwtFilter.java
@@ -4,7 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.data.redis.core.RedisTemplate;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,12 +17,12 @@ import java.io.IOException;
 public class JwtFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final MyUserDetailsService myUserDetailsService;
-    private final RedisTemplate<String,Object>redisTemplate;
 
-    public JwtFilter(JwtService jwtService, MyUserDetailsService myUserDetailsService, RedisTemplate<String, Object> redisTemplate) {
+
+    public JwtFilter(JwtService jwtService, MyUserDetailsService myUserDetailsService) {
         this.jwtService = jwtService;
         this.myUserDetailsService = myUserDetailsService;
-        this.redisTemplate = redisTemplate;
+
     }
 
 
@@ -36,19 +36,6 @@ public class JwtFilter extends OncePerRequestFilter {
         }
 
         String token = header.substring(7).trim();
-
-
-
-        String redisKey = "BL_" + token;
-
-
-
-
-        if (Boolean.TRUE.equals(redisTemplate.hasKey(redisKey))) {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.getWriter().write("Token is blocked");
-            return;
-        }
 
         String username = jwtService.extractByUsername(token);
             if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
